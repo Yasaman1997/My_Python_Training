c1b131e7a973de8af199195cffb20c91f0935eab
Yasaman <y.mirmohammad@yahoo.com>
2017-06-28 14:58:45 +0430

    Add files via upload

---
+++ Test/odd or even/__init__.py
@@ -0,0 +1,5 @@
+num = int(input("give a number to check: \n "))
+check = int(input("give me a number to divide by: \n"))
+
+if num%check == 0:
+   print( num, "is a multiple of " , check)
---
+++ Test/till you get 100/__init__.py
@@ -0,0 +1,6 @@
+
+
+name = input("enter your name: \n")
+age = int(input(" your age : \n  "))
+year = str((2017-age)+100)
+print(name + " will be 100 years old in the year " +year)
