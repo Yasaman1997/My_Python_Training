26618007d19bbea3293d2d54610db8aec93ae57e
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2017-11-20 23:58:53 +0330

    Delete Better.py

--- Practice/Practice/Practice/Password_Generator/Better.py
+++
@@ -1,8 +0,0 @@
-
-import string
-import random
-
-def pw_gen(size = 8, chars=string.ascii_letters + string.digits + string.punctuation):
-	return ''.join(random.choice(chars) for _ in range(size))
-
-print(pw_gen(int(input('How many characters in your password?'))))
